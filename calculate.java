c650feaaf7bd19714627d8a5698980866ab9b712
sam <samueltkooy@gmail.com>
2018-05-11 13:49:03 +0100

    add java file

---
+++ calculate.java
@@ -0,0 +1,8 @@
+public class Calculate {
+   public static void main(String[] args){
+      int var1 = 2;
+      int var2 = 3;
+      var1 = var1 * var2;
+      System.out.println("2 times 3 is " + var1);
+   }
+}
