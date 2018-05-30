b00f60358860feded9bf229b1f154ac58f7e2651
samtk <38210173+samtk@users.noreply.github.com>
2018-05-30 15:56:07 +0100

    Update calculate.java

--- calculate.java
+++ calculate.java
@@ -1,8 +1,8 @@
 public class calculate {
    public static void main(String[] args){
       int var1 = 2;
-      int var2 = 3;
+      int var2 = 4;
       var1 = var1 * var2;
-      System.out.println("2 times 3 is: " + var1);
+      System.out.println("2 times 4 is: " + var1);
    }
 }
